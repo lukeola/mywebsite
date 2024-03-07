a01622a6d81d70258013d34457343f1774382dbd
Luke Olawale <lukeolawale4@gmail.com>
2024-03-07 18:47:15 +0100

    update profile

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,51 +1,51 @@
-import React from "react";
-import "./experience.css";
-import { BsPatchCheckFill } from "react-icons/bs";
+import React from 'react';
+import './experience.css';
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 const experience = () => {
   return (
-    <section id="experience">
+    <section id='experience'>
       <h5>My Skills</h5>
       <h2>My Experience</h2>
 
-      <div className="container experience__container">
+      <div className='container experience__container'>
         {/* ==================FRONT END EXPERIENCE BEGINS =====================*/}
-        <div className="experience_frontend">
+        <div className='experience_frontend'>
           <h3>Fronted Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>HTML</h4>
-                <small className="text-light"> Experienced</small>
+                <small className='text-light'> Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>TAILWIND CSS</h4>
-                <small className="text-light"> Experienced</small>
+                <small className='text-light'> Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>NEXT JS</h4>
-                <small className="text-light"> Experienced</small>
+                <small className='text-light'> Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>REACT NATIVE</h4>
-                <small className="text-light"> Experienced</small>
+                <small className='text-light'> Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>REACT</h4>
-                <small className="text-light"> Experienced</small>
+                <small className='text-light'> Experienced</small>
               </div>
             </article>
           </div>
@@ -53,42 +53,42 @@ const experience = () => {
         {/* =================END OF FRONT END EXPERIENCE =============================*/}
 
         {/* ===================BACKEND EXPERIENCE BEGINS=========================== */}
-        <div className="experience__backend">
+        <div className='experience__backend'>
           <h3>Backend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>PYTHON</h4>
-                <small className="text-light"> Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>POSTGRESQL</h4>
-                <small className="text-light"> Experienced</small>
+                <small className='text-light'> Intermediate</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>PHP</h4>
-                <small className="text-light">Intermediate</small>
+                <h4>MYSQL</h4>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>EXPRESS JS</h4>
-                <small className="text-light">Intermediate</small>
+                <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
+                <h4>PYTHON</h4>
+                <small className='text-light'> Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>NODE JS</h4>
-                <small className="text-light"> Experienced</small>
+                <small className='text-light'> Experienced</small>
               </div>
             </article>
           </div>
--- src/components/header/AnimatedText.jsx
+++ src/components/header/AnimatedText.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from 'react';
 import Typewriter from 'typewriter-effect';
 
 const AnimatedText = () => {
   return (
-    <div>  <Typewriter
-    onInit={(typewriter) => {
-      
-      typewriter.typeString("Hello I'm")
-        .callFunction(() => {
-          // console.log('String typed out!');
-        })
-        
-        .callFunction(() => {
-          console.log('All strings were deleted');
-        })
-        .start()
-        
-       
+    <div>
+      {' '}
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .typeString("Hello I'm")
+            .callFunction(() => {
+              // console.log('String typed out!');
+            })
 
-     
-    }}
-  /></div>
-  )
-}
+            .callFunction(() => {
+              console.log('All strings were deleted');
+            })
+            .start();
+        }}
+      />
+    </div>
+  );
+};
 
-export default AnimatedText
+export default AnimatedText;
